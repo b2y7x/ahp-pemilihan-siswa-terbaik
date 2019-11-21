e304bee21e7efdce46a5d299508b57f1b841a4b0
BYX <bayubimantoro27@gmail.com>
2019-11-21 23:30:32 +0700

    fix lagi

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('username')->unique();
+            $table->string('username')->unique()->nullable();
             $table->string('email')->unique();
-            $table->string('no_telp', 20);
-            $table->boolean('is_admin');
+            $table->string('no_telp', 20)->nullable();
+            $table->boolean('is_admin')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
